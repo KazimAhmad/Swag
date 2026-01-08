536f45c0962767cf8249410267e5bb6a3f72b10f
Kazim Ahmad <bitchy_kazim@Kazims-MacBook-Pro.local>
2026-01-08 01:08:54 +0000

    clean up and shared views files

--- app/Swag/Swag/Auth/Login/LoginView.swift
+++ app/Swag/Swag/Auth/Login/LoginView.swift
@@ -8,7 +8,6 @@
 import SwiftUI
 
 struct LoginView<ViewModel: LoginViewModelProtocol>: View {
-    @Environment(\.colorScheme) private var colorScheme
     @StateObject private var viewModel: ViewModel
     
     init(viewModel: ViewModel) {
@@ -16,45 +15,16 @@ struct LoginView<ViewModel: LoginViewModelProtocol>: View {
     }
     
     var body: some View {
-        VStack {
-            lampHeader()
-            Spacer()
-        }
-        .ignoresSafeArea()
-    }
-    
-    func lampHeader() -> some View {
-        HStack {
-            Spacer()
-            VStack(spacing: 0) {
-                Image(Images.lamp)
-                    .resizable()
-                    .frame(width: 100, height: 100)
-                Triangle()
-                    .frame(width: 350, height: 200, alignment: .center)
-                    .foregroundStyle(
-                        LinearGradient(colors: viewModel.lampColors(for: colorScheme),
-                                       startPoint: .top,
-                                       endPoint: .bottom)
-                    )
-                    .offset(y: -16)
+        ZStack {
+            LampAndLightView()
+            VStack {
+                
             }
         }
-        .offset(x: 80)
+        .ignoresSafeArea()
     }
 }
 
 #Preview {
     LoginView(viewModel: LoginViewModel())
-}
-
-struct Triangle : Shape{
-    func path(in rect: CGRect) -> Path {
-        Path { path in
-            path.move(to: CGPoint(x: rect.midX, y: rect.minY))
-            path.addLine(to: CGPoint(x: rect.maxX, y: rect.maxY))
-            path.addLine(to: CGPoint(x: rect.minX, y: rect.maxY))
-            path.closeSubpath()
-        }
-    }
 }
--- app/Swag/Swag/Auth/Login/LoginViewModel.swift
+++ app/Swag/Swag/Auth/Login/LoginViewModel.swift
@@ -9,20 +9,7 @@ import Foundation
 import SwiftUI
 
 protocol LoginViewModelProtocol: ObservableObject {
-    func lampColors(for colorScheme: ColorScheme) -> [Color]
 }
 
 class LoginViewModel: LoginViewModelProtocol {
-    func lampColors(for colorScheme: ColorScheme) -> [Color] {
-        if colorScheme == .dark {
-            return [.lampLight,
-                    .lampLight.opacity(0.8),
-                    .lampLight.opacity(0.6),
-                    .lampLight.opacity(0.4),
-                    .lampLight.opacity(0.2),
-                    .lampLight.opacity(0.1),
-                    .lampLight.opacity(0.0)]
-        }
-        return []
-    }
 }
