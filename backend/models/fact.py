8abd00c5dd53e0d9a0d021ac5f816d60af7736f6
Kazim Ahmad <bitchy_kazim@Kazims-MacBook-Pro.local>
2026-01-28 04:28:19 +0000

    fact model updates

--- backend/models/fact.py
+++ backend/models/fact.py
@@ -1,3 +1,5 @@
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.orm import relationship
 from config.config import db
 from datetime import datetime
 
@@ -8,6 +10,8 @@ class Fact(db.Model):
     date = db.Column(db.DateTime, nullable = False, default= datetime.now(datetime.timezone.utc))
     link = db.Column(db.String(120), nullable = True)
 
+    category_id = Column(Integer, ForeignKey("categories.id"))
+    category = relationship("Category", back_populates="facts")
     def __repr__(self):
         return f"Fact machine ('{self.title}', '{self.description}')"
     
@@ -17,6 +21,19 @@ class Fact(db.Model):
             "title": self.title,
             "description": self.description,
             "link": self.link,
-            "date": self.date
+            "date": self.date,
+            "category": self.category
         }
     
+
+class FactCategory(db.Model):
+    id = Column(Integer, primary_key=True)
+    name = Column(String, nullable=False)
+
+    facts = relationship("Fact", back_populates="category")
+
+    def to_json(self):
+        return {
+            "id": self.id,
+            "name": self.name
+        }
--- backend/requests/fact.py
+++ backend/requests/fact.py
@@ -4,17 +4,22 @@ sys.path.append("../config/config.py")
 sys.path.append("../models/video.py")
 
 from config.config import db, app
-from models.fact import Fact
+from models.fact import Fact, FactCategory
 from datetime import datetime
 
 @app.route("/facts", methods = ["GET"])
 def facts():
-    facts = Fact.query.all()
-    facts_to_json = list(map(lambda fact: fact.to_json(), facts))
+    page_number = request.args.get("page", default=1, type=int)
+    facts = Fact.query.order_by(
+        Fact.date.desc()
+    ).paginate(per_page=5, page=page_number)
 
+    facts_to_json = list(map(lambda thought: thought.to_json(), facts.items))
+    
     return jsonify(
         {
-            "facts": facts_to_json
+            "total": facts.total,
+            "items": facts_to_json
         }
     )
 
@@ -23,6 +28,10 @@ def fact_create():
     new_title = request.json.get("title")
     new_description = request.json.get("description")
     new_link = request.json.get("link")
+    new_fact_category = request.json.get("category")
+    new_fact_category_name = new_fact_category("name")
+
+    fact_category = FactCategory(new_fact_category_name)
 
     if not new_title:
         return jsonify(
@@ -31,10 +40,18 @@ def fact_create():
             }
         ), 400
     
+    if not fact_category:
+        return jsonify(
+            {
+                "error": "A valid category is required"
+            }
+        ), 400
+    
     new_fact = Fact(title = new_title,
                     description = new_description,
                     link = new_link,
-                    date = datetime.now(datetime.timezone.utc))
+                    date = datetime.now(datetime.timezone.utc),
+                    category = fact_category)
     
     try:
         db.session.add(new_fact)
@@ -49,6 +66,69 @@ def fact_create():
     
     return jsonify({"message": "fact created"}), 200
 
+@app.route("/facts", methods = ["DELETE"])
+def delete_facts():
+    data = request.get_json()
+    ids = data.get("ids", [])
 
+    if not ids:
+        return jsonify({"error": "No IDs provided"}), 400
 
+    facts = Fact.query.filter(Fact.id.in_(ids)).all()
 
+    if not facts:
+        return jsonify({"error": "No facts found"}), 404
+    
+    for fact in facts:
+        db.session.delete(fact)
+
+    db.session.commit()
+
+    return jsonify({
+        "deleted_ids": [t.id for t in facts]
+    }), 200
+
+@app.route("/facts/category/create", methods = ["POST"])
+def fact_category_create():
+    new_name = request.json.get("name")
+
+    same_category = FactCategory.query.filter_by(new_name).first()
+    if same_category:
+        return jsonify(
+            {
+                "error": "a fact category with same name already exists"
+            }
+        ), 400
+    
+    if not new_name:
+        return jsonify(
+            {
+                "error": "A valid name is required"
+            }
+        ), 400
+    
+    new_fact_category = FactCategory(title = new_name)
+    
+    try:
+        db.session.add(new_fact_category)
+        db.session.commit()
+    
+    except Exception as e:
+        return jsonify(
+            {
+                "error": str(e) 
+            }
+        ), 400
+    
+    return jsonify({"message": "fact category created"}), 200
+
+@app.route("/facts/categories", methods = ["GET"])
+def facts_categories():
+    facts_cats = FactCategory.query.all()
+    facts_to_json = list(map(lambda fact: fact.to_json(), facts_cats))
+
+    return jsonify(
+        {
+            "facts": facts_to_json
+        }
+    )
